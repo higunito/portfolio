4d4e3e4f108b4378b6fb114233a10a5dfbc84048
higunito <reisen0615akira@gmail.com>
2025-02-24 22:15:55 +0900

    add Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
 const Footer = () => {
   return (
-    <>
-    
-    </>
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto text-center">
+        <p>Maintained by Taro Yamada</p>
+        <p>Last updated in Feb. 2025.</p>
+      </div>
+    </footer>
   );
 };
 
