6129f0cfe1a909b3f6813e9121fa80ed6ffb5dd5
higunito <reisen0615akira@gmail.com>
2025-02-26 07:32:13 +0900

    remove gsap dependency and related animation code from Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,8 +2,6 @@
 
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Stars } from '@react-three/drei';
-import { useEffect } from 'react';
-import gsap from 'gsap';
 import About from './About';
 import Experience from './Experience';
 import Portfolio from './Portfolio';
@@ -11,14 +9,6 @@ import Blog from './Blog';
 import Contact from './Contact';
 
 const Main = () => {
-  useEffect(() => {
-    gsap.fromTo(
-      '.content',
-      { opacity: 0, y: 50 },
-      { opacity: 1, y: 0, duration: 1, stagger: 0.3 }
-    );
-  }, []);
-
   return (
     <main className="pt-24 relative">
       <Canvas className="absolute top-0 left-0 w-full h-full z-0">
@@ -27,7 +17,7 @@ const Main = () => {
         <Stars />
         <OrbitControls />
       </Canvas>
-      <div className="relative z-10 content">
+      <div className="relative z-10">
         <About />
         <Experience />
         <Portfolio />
